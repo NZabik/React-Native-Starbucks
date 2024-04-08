84ae1c90ffe42c9db829498fe082863bd513a924
Nicolas Zabik <melchiah.nico@gmail.com>
2024-04-08 08:16:27 +0200

    modification app.js

--- App.js
+++ App.js
@@ -2,24 +2,6 @@ import { StyleSheet } from 'react-native';
 import Navigation from './Navigation'
 
 export default function App() {
-  // const [navType, setNavType] = useState(null);
-// 
-  // if (navType === 'tabs') {
-  //   return <Tabs setNavType={setNavType} />;
-  // } else if (navType === 'drawer') {
-  //   return <Navigation setNavType={setNavType} />;
-  // } else {
-  //   return (
-  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-  //       <View style={styles.button}>
-  //         <Button color='#A7000C' title="Utiliser la navigation par onglets" onPress={() => setNavType('tabs')} />
-  //       </View>
-  //       <View style={styles.button}>
-  //         <Button color='#A7000C' title="Utiliser la navigation par tiroir" onPress={() => setNavType('drawer')} />
-  //       </View>
-  //     </View>
-  //   );
-  // }
   return <Navigation />;
 }
 
